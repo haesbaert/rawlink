04a349b06981fa2f46c20c89a58a1ef78caaccd0
Christiano Haesbaert <haesbaert@haesbaert.org>
2015-08-14 19:24:04 +0200

    Add put_packet.

--- lib/rawlink.ml
+++ lib/rawlink.ml
@@ -5,6 +5,7 @@ type t = {
 
 external opensock: ?filter:string -> string -> Unix.file_descr = "caml_rawlink_open"
 external readsock: Unix.file_descr -> string list = "caml_rawlink_read"
+external writesock: Unix.file_descr -> string -> unit = "caml_rawlink_write"
 external dhcp_filter: unit -> string = "caml_dhcp_filter"
 
 let open_link ?filter ifname =
@@ -19,3 +20,9 @@ let rec get_packet t = match !(t.packets) with
 let get_packet_list t = match !(t.packets) with
   | [] -> readsock t.fd
   | packets -> t.packets := []; packets
+
+let put_packet t b =
+  let len = Bytes.length b in
+  let n = Unix.write t.fd b 0 len in
+  if n <> len then
+    raise (Unix.Unix_error(Unix.ENOBUFS, "put_packet", ""))
--- lib/rawlink.mli
+++ lib/rawlink.mli
@@ -4,4 +4,5 @@ val open_link : ?filter:string -> string -> t
 val close_link : t -> unit
 val get_packet : t -> string
 val get_packet_list : t -> string list
+val put_packet : t -> string -> unit
 val dhcp_filter : unit -> string
