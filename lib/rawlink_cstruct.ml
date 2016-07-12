4859961314cda21e43d10f1a5708cffefe1108f2
Christiano Haesbaert <haesbaert@haesbaert.org>
2016-07-12 15:40:06 +0200

    Convert to ppx.

--- lib/rawlink_cstruct.ml
+++ lib/rawlink_cstruct.ml
@@ -1,7 +1,8 @@
-cstruct bpf_hdr {
-	uint32_t     bh_sec;
-	uint32_t     bh_usec;
-	uint32_t     bh_caplen;
-	uint32_t     bh_datalen;
-	uint16_t     bh_hdrlen;
-} as little_endian
+[%%cstruct
+type bpf_hdr = {
+	bh_sec: uint32_t;
+	bh_usec: uint32_t;
+	bh_caplen: uint32_t;
+	bh_datalen: uint32_t;
+	bh_hdrlen: uint16_t;
+} [@@little_endian]]
