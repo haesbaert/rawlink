4f659a18eaab273c8a014e6f27005ae3c1f88e9c
gabrik <gabriele.baldoni@gmail.com>
2019-01-30 14:22:46 +0100

    fixes and comment addressing
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- lib/rawlink_stubs.c
+++ lib/rawlink_stubs.c
@@ -131,6 +131,26 @@ int bpf_setimmediate(int fd, u_int opt) {
   return (r);
 }
 
+/*
+ * BPF is better designed than AF_PACKET, promisc is unset once all bpf sessions
+ * to that interface are closed, so we don't need to clear it
+ */
+int bpf_setpromisc(int fd, value vpromisc) {
+  int r;
+
+  if (!Bool_val(vpromisc))
+    return (0);
+
+  caml_enter_blocking_section();
+  r = ioctl(fd, BIOCPROMISC, NULL);
+  caml_leave_blocking_section();
+
+  if (r == 1)
+    uerror("bpf_setpromisc", Nothing);
+
+  return (r);
+}
+
 int bpf_setfilter(int fd, value vfilter) {
   int r;
   struct bpf_program prog;
@@ -150,8 +170,8 @@ int bpf_setfilter(int fd, value vfilter) {
   return (r);
 }
 
-CAMLprim value caml_rawlink_open(value vpromisc, value vfilter, value vifname) {
-  CAMLparam3(vpromisc, vfilter, vifname);
+CAMLprim value caml_rawlink_open(value vfilter, value vpromisc, value vifname) {
+  CAMLparam3(vfilter, vpromisc, vifname);
   int fd;
 
   if ((fd = bpf_open()) == -1)
@@ -165,6 +185,8 @@ CAMLprim value caml_rawlink_open(value vpromisc, value vfilter, value vifname) {
   if (bpf_setif(fd, String_val(vifname)) == -1)
     CAMLreturn(Val_unit);
   if (bpf_setimmediate(fd, 1) == -1)
+    CAMLreturn(Val_unit);
+  if (bpf_setpromisc(fd, 1) == -1)
     CAMLreturn(Val_unit);
 
   CAMLreturn(Val_int(fd));
@@ -247,6 +269,7 @@ int af_packet_setfilter(int fd, value vfilter) {
   return (r);
 }
 
+/* 1 for set, 0 for clear */
 int af_packet_setpromisc(int fd, int promisc, int setclr) {
   int r;
   struct ifreq ifreq;
@@ -282,8 +305,8 @@ int af_packet_setpromisc(int fd, int promisc, int setclr) {
   return (r);
 }
 
-CAMLprim value caml_rawlink_open(value vpromisc, value vfilter, value vifname) {
-  CAMLparam3(vpromisc, vfilter, vifname);
+CAMLprim value caml_rawlink_open(value vfilter, value vpromisc, value vifname) {
+  CAMLparam3(vfilter, vpromisc, vifname);
   int fd;
 
   if ((fd = af_packet_open()) == -1)
@@ -306,13 +329,31 @@ CAMLprim value caml_bpf_align(value va, value vb) {
 
 #endif /* USE_AF_PACKET */
 
-CAMLprim value caml_driver(value vunit) {
-  CAMLparam0();
-#ifdef AF_PACKET
+CAMLprim value caml_rawlink_close(value vfd, value vpromisc) {
+  CAMLparam2(vfd, vpromisc);
+  int r;
+
+#ifdef USE_AF_PACKET
   /*
    * Ignore return on purpose, doing the close is more relevant.
    */
   (void)af_packet_setpromisc(Int_val(vfd), Bool_val(vpromisc), 0);
+#endif
+
+  caml_enter_blocking_section();
+  r = close(Int_val(vfd));
+  caml_leave_blocking_section();
+
+  if (r == -1)
+    uerror("caml_rawlink_close", Nothing);
+
+  CAMLreturn(Val_unit);
+}
+
+CAMLprim value caml_driver(value vunit) {
+  CAMLparam0();
+#ifdef AF_PACKET
+  CAMLreturn(Val_int(0));
 #else
   CAMLreturn(Val_int(1));
 #endif
