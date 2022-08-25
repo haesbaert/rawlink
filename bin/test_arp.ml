4f3a43b1b2abe92ef6a6300cd08c31c527f26668
Christiano Haesbaert <haesbaert@haesbaert.org>
2022-08-25 14:12:36 +0200

    Improve arp test with reply

--- bin/test_arp.ml
+++ bin/test_arp.ml
@@ -1,6 +1,6 @@
 let a_mac = Macaddr.of_string_exn "18:18:18:18:18:18"
 let a_ip = Ipaddr.V4.of_string_exn "18.18.18.18"
-let _b_mac = Macaddr.of_string_exn "29:29:29:29:29:29"
+let b_mac = Macaddr.of_string_exn "29:29:29:29:29:29"
 let b_ip = Ipaddr.V4.of_string_exn "29.29.29.29"
 
 let a_arp_request =
@@ -8,7 +8,16 @@ let a_arp_request =
   let source_ip = a_ip in
   let target_mac = Macaddr.of_string_exn "00:00:00:00:00:00" in
   let target_ip = b_ip in
-  let arp = Arp_packet.{ operation = Arp_packet.Request; source_mac; source_ip; target_mac; target_ip } in
+  let arp = Arp_packet.{ operation = Request; source_mac; source_ip; target_mac; target_ip } in
+  let eth = Ethernet.Packet.{ source = source_mac; destination = Macaddr.broadcast; ethertype = `ARP } in
+  Cstruct.append (Ethernet.Packet.make_cstruct eth) (Arp_packet.encode arp)
+
+let b_arp_reply =
+  let source_mac = b_mac in
+  let source_ip = b_ip in
+  let target_mac = b_mac in
+  let target_ip = a_ip in
+  let arp = Arp_packet.{ operation = Reply; source_mac; source_ip; target_mac; target_ip } in
   let eth = Ethernet.Packet.{ source = source_mac; destination = Macaddr.broadcast; ethertype = `ARP } in
   Cstruct.append (Ethernet.Packet.make_cstruct eth) (Arp_packet.encode arp)
 
@@ -17,23 +26,31 @@ let pp hdr buf =
   Cstruct.hexdump buf;
   print_newline ()
 
+let send link hdr pkt =
+  Eio_rawlink.send_packet link
+    (Eio.Flow.cstruct_source [ pkt ]);
+  pp hdr pkt
+
+let recv link hdr =
+  let pkt = Eio_rawlink.read_packet link in
+  pp hdr pkt;
+  pkt
+
+let rec wait_pkt link hdr pkt =
+  let pkt' = recv link hdr in
+  if not (Cstruct.equal pkt pkt') then
+    wait_pkt link hdr pkt
+
 let () =
   Eio_main.run @@ fun _env ->
   Eio.Switch.run (fun sw ->
-      let a_link = Eio_rawlink.open_link ~sw ~promisc:true "lo" in
-      let b_link = Eio_rawlink.open_link ~sw ~promisc:true "lo" in
+    let a_link = Eio_rawlink.open_link ~sw ~promisc:true "lo" in
+    let b_link = Eio_rawlink.open_link ~sw ~promisc:true "lo" in
 
-      (* Make an arp request on a, answer from b *)
-      Eio_rawlink.send_packet a_link (Eio.Flow.cstruct_source [ a_arp_request ]);
-      pp "A sent:" a_arp_request;
-      Eio_rawlink.close_link a_link;
-      let rec loop () =
-        let pkt = Eio_rawlink.read_packet b_link in
-        pp "B got:" pkt;
-        if Cstruct.equal pkt a_arp_request then
-          let () = Eio_rawlink.close_link b_link in
-          Printf.printf "match !\n%!"
-        else
-          loop ()
-      in
-      loop ())
+    send a_link "A sent:" a_arp_request;
+    wait_pkt b_link "B got:" a_arp_request;
+    Printf.printf "request match !\n%!";
+
+    send b_link "B sent:" b_arp_reply;
+    wait_pkt a_link "A got:" b_arp_reply;
+    Printf.printf "reply match !\n%!")
